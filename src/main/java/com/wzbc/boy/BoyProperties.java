053e9310471b8481617bad2a209eb83204dad2e6
JasonStephen <b1685941616@gmail.com>
2023-06-06 17:01:31 +0800

    Subject Project

---
+++ src/main/java/com/wzbc/boy/BoyProperties.java
@@ -0,0 +1,31 @@
+package com.wzbc.boy;
+
+public class BoyProperties {
+private String cupSize;
+    private Integer age;
+    private String content;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- src/main/java/com/wzbc/boy/HelloController.java
+++ src/main/java/com/wzbc/boy/HelloController.java
@@ -1,5 +1,6 @@
 package com.wzbc.boy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,33 @@ import java.util.Map;
 
 @RestController
 public class HelloController {
+    @Value("${cupSize}")
+    private Integer cupSize;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${content}")
+    private String content;
+
+    @GetMapping("/getCupSize")
+    public String getCupSize() {
+        return "My cup size is:" + cupSize;
+    }
+
+    @GetMapping("/getInfo")
+    public Map getInfo() {
+        Map map = new HashMap();
+        map.put("name", name);
+        map.put("age", age);
+        map.put("cupSize", cupSize);
+        map.put("content", content);
+        return map;
+    }
+
     @GetMapping("/sayhello")
     public Map sayHello() {
         Map map = new HashMap();
